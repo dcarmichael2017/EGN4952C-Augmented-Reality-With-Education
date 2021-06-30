bda41b3c85d89bbbf534eb758cbfa5648db18965
dcarmichael2017 <dcarmichael2017@fau.edu>
2021-06-30 10:25:01 -0400

    Updated spawning system and health system.
    -Need to add ragdoll using unity ragdoll wizard, instantiate a new gameobject of the prefab using the ragdoll wizard and delete old.
    -Allow health to finish animating before destroying old gameobject
    -Add health to castle on collision

---
+++ Assets/Scripts/Level 1/EnemyController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+    private int currentHealth;
+
+    public GameObject healthBarCanvas;
+
+    //null check
+    public event Action<float> OnHealthPctChanged = delegate { };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBarCanvas.gameObject.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth < maxHealth)
+            healthBarCanvas.gameObject.transform.localScale = new Vector3((float)0.01, (float)0.01, (float)0.01);
+
+        if (currentHealth <= 0)
+            Destroy(gameObject);
+    }
+
+    public void ModifyHealth(int amount) 
+    {
+        currentHealth += amount;
+
+        float currentHealthPct = (float)currentHealth / (float)maxHealth;
+        OnHealthPctChanged(currentHealthPct);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Projectile")
+        {
+            ModifyHealth(-50);
+            
+        }
+    }
+}
