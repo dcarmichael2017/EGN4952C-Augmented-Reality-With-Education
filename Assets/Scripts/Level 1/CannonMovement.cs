997b9dc13b15b194cd9cf4a6badef0953a001c76
Jordan Barrios <jbarrios2017@fau.edu>
2021-07-31 12:33:49 -0400

    Small accelerometer fix
    Non-AR camera now follows cannon properly when using accelerometer.

--- Assets/Scripts/Level 1/CannonMovement.cs
+++ Assets/Scripts/Level 1/CannonMovement.cs
@@ -64,6 +64,12 @@ public class CannonMovement : MonoBehaviour
         rb.transform.rotation = Quaternion.Euler(rb.transform.rotation.eulerAngles + new Vector3(0, 0, dirX * 1f));
         barrel.transform.rotation = Quaternion.Euler(new Vector3(barrel.transform.rotation.eulerAngles.x, rb.transform.rotation.eulerAngles.y, rb.transform.rotation.eulerAngles.z) + new Vector3(0, 0, dirX * 1f)); //use the cannon's y and z rotation
 
+        //only rotate camera if not using augmented reality
+        if (cameraRotator != null)
+        {
+            cameraRotator.transform.rotation = Quaternion.Euler(cameraRotator.transform.rotation.eulerAngles + new Vector3(0, dirX * 1f, 0));
+        }
+
     }
 
     public void Buttons(string buttons) 
