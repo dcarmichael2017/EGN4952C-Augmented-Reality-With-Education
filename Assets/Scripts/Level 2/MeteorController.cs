b6c75885d31d664dac6a48c53179ad5f94627aa5
Jordan Barrios <jbarrios2017@fau.edu>
2021-08-02 11:38:00 -0400

    Added difficulty changes to level 2
    High difficulties increase damage dealt to castle.

--- Assets/Scripts/Level 2/MeteorController.cs
+++ Assets/Scripts/Level 2/MeteorController.cs
@@ -7,10 +7,24 @@ public class MeteorController : MonoBehaviour
 
     public CastleController castleController;
     public CastleHealth castleHealth;
+
+    private int difficultyLevel = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set difficultyLevel (higher level increases damage dealt to castle)
+        if (GameValues.currentDifficulty == GameValues.Difficulties.Easy)
+        {
+            difficultyLevel = 1;
+        }
+        else if (GameValues.currentDifficulty == GameValues.Difficulties.Medium)
+        {
+            difficultyLevel = 2;
+        }
+        else if (GameValues.currentDifficulty == GameValues.Difficulties.Hard)
+        {
+            difficultyLevel = 3;
+        }
     }
 
     // Update is called once per frame
@@ -28,7 +42,7 @@ public class MeteorController : MonoBehaviour
             if (castleController.WithinRange(this.transform))
             {
                 FindObjectOfType<AudioManager>().Play("MeteorCollide");
-                castleHealth.ModifyHealth(-10);
+                castleHealth.ModifyHealth(-10 * difficultyLevel);
                 //Destroy cannonball on collision
                 Destroy(gameObject);
             }
